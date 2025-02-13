9a02cec1572e1140254fff2775fe90ebe0d7b4bc
Biaguy Niakate <n.biaguy@gmail.com>
2025-02-13 23:10:45 +0100

    Ajout des states, d'useDispatch et useSelector au composant Login.jsx

--- Front-end/src/page/login/Login.jsx
+++ Front-end/src/page/login/Login.jsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from '../../features/auth/authSlice';
+import { useState } from 'react';
 
 const SignInContainer = styled.div`
   display: block;
@@ -54,48 +57,90 @@ const InputRemember = styled.div`
 `;
 
 const LabelRemember = styled.label`
-font-weight: 300;
-`
+  font-weight: 300;
+`;
 
 const SignInButton = styled.button`
-    display: block;
-    width: 100%;
-    padding: 8px;
-    font-size: 1.1rem;
-    font-weight: 400;
-    text-decoration: underline;
-    margin-top: 1rem;
-    border: none;
-    background-color: #00bc77;
-    color: #fff;
+  display: block;
+  width: 100%;
+  padding: 8px;
+  font-size: 1.1rem;
+  font-weight: 400;
+  text-decoration: underline;
+  margin-top: 1rem;
+  border: none;
+  background-color: #00bc77;
+  color: #fff;
 `;
 
 export const Login = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
+  // On récupère les états depuis le store Redux
+  const { isLoading, error } = useSelector(state => state.auth);
+
+  // États locaux pour le formulaire
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleSubmit = async e => {
     e.preventDefault();
-    navigate('/profile');
+
+    // On dispatch notre action loginUser avec les identifiants
+    const result = await dispatch(loginUser({ email, password }));
+
+    // Si la connexion réussit, on redirige vers le profil
+    if (result.meta.requestStatus === 'fulfilled') {
+      navigate('/profile');
+    }
   };
+
   return (
     <SignInContainer>
       <MainBgDark>
         <SignInContent onSubmit={handleSubmit}>
           <Icon className="fa fa-user-circle" />
           <SignInTitle>Sign In</SignInTitle>
+
+          {/* Affichage des erreurs */}
+          {error && <div style={{ color: 'red', marginBottom: '1rem' }}>{error}</div>}
+
           <InputWrapper>
             <Label htmlFor="username">Username</Label>
-            <Input type="text" id="username" autoComplete="username" />
+            <Input
+              type="email"
+              id="username"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              autoComplete="username"
+            />
           </InputWrapper>
+
           <InputWrapper>
             <Label htmlFor="password">Password</Label>
-            <Input type="password" id="password" autoComplete="current-password" />
+            <Input
+              type="password"
+              id="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              autoComplete="current-password"
+            />
           </InputWrapper>
+
           <InputRemember>
-            <Input type="checkbox" />
-            <LabelRemember htmlFor="remerber-me">Remember me</LabelRemember>
+            <Input
+              type="checkbox"
+              checked={rememberMe}
+              onChange={e => setRememberMe(e.target.checked)}
+            />
+            <LabelRemember htmlFor="remember-me">Remember me</LabelRemember>
           </InputRemember>
-          <SignInButton type='submit'>Sign In</SignInButton>
+
+          <SignInButton type="submit" disabled={isLoading}>
+            {isLoading ? 'Loading...' : 'Sign In'}
+          </SignInButton>
         </SignInContent>
       </MainBgDark>
     </SignInContainer>
